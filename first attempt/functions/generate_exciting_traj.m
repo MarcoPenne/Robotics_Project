5adaa5e3f75e12937c59c2c6b17ac4141d19b0f1
MarcoPenne <marco.pennese.97@gmail.com>
2020-09-08 13:12:33 +0200

    "working" 3-dof

--- first attempt/functions/generate_exciting_traj.m
+++ first attempt/functions/generate_exciting_traj.m
@@ -6,15 +6,15 @@ limit = 1;
 a = rand(5,1) -0.5;
 b = rand(5,1) -0.5;
 q0 = rand(1,1) - 0.5;
-w = 0.3*pi;
+w = 0.1*pi;
 
 syms pos(t);
 
-pos = @(t) (a(1)/w)*sin(w*t) - (b(1)/w)*cos(w*t) ...
-         + (a(2)/(2*w))*sin(2*w*t) - (b(2)/(2*w))*cos(2*w*t) + q0(1);
-         %+ (a(3)/(3*w))*sin(3*w*t) - (b(3)/(3*w))*cos(3*w*t) ...
-         %+ (a(4)/(4*w))*sin(4*w*t) - (b(4)/(4*w))*cos(4*w*t) ...
-         %+ (a(5)/(5*w))*sin(5*w*t) - (b(5)/(5*w))*cos(5*w*t) ...
+pos = @(t) q0(1) + (a(1)/w)*sin(w*t) - (b(1)/w)*cos(w*t) ...
+         + (a(2)/(2*w))*sin(2*w*t) - (b(2)/(2*w))*cos(2*w*t) ...
+         + (a(3)/(3*w))*sin(3*w*t) - (b(3)/(3*w))*cos(3*w*t) ...
+         + (a(4)/(4*w))*sin(4*w*t) - (b(4)/(4*w))*cos(4*w*t) ...
+         + (a(5)/(5*w))*sin(5*w*t) - (b(5)/(5*w))*cos(5*w*t);
 g = diff(pos, t);
 i=1;
 sol = [];
@@ -25,10 +25,19 @@ for n = 1:20
 end
 extrema = vpa(sol, 6);
 maximum = max(abs(pos(extrema)));
-
 pos = @(t) pos(t)*limit/maximum;
 vel_tmp(t) = diff(pos, t);
 vel = @(t) double(vel_tmp(t));
 acc_tmp(t) = diff(vel_tmp, t);
 acc = @(t) double(acc_tmp(t));
+
+time = 0:0.02:((2*pi)/w);
+save('time.mat', 'time');
+position = double(pos(time));
+save('pos.mat', 'position');
+velocity = double(vel(time));
+save('vel.mat', 'velocity');
+acceleration = double(acc(time));
+save('acc.mat', 'acceleration');
+
 end
