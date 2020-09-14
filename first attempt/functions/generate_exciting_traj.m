99f60babe78c741ce82aef7aac96701d776f3718
MarcoPenne <marco.pennese.97@gmail.com>
2020-09-14 15:02:58 +0200

    upgraded trajectory generator

--- first attempt/functions/generate_exciting_traj.m
+++ first attempt/functions/generate_exciting_traj.m
@@ -1,8 +1,8 @@
-function [pos,vel, acc] = generate_exciting_traj()
+function [pos,vel, acc] = generate_exciting_traj(i)
 %GENERATE_EXCITING_TRAJ Summary of this function goes here
 %   Detailed explanation goes here
 
-limit = 1;
+limit = pi/6;
 a = rand(5,1) -0.5;
 b = rand(5,1) -0.5;
 q0 = rand(1,1) - 0.5;
@@ -15,10 +15,13 @@ pos = @(t) q0(1) + (a(1)/w)*sin(w*t) - (b(1)/w)*cos(w*t) ...
          + (a(3)/(3*w))*sin(3*w*t) - (b(3)/(3*w))*cos(3*w*t) ...
          + (a(4)/(4*w))*sin(4*w*t) - (b(4)/(4*w))*cos(4*w*t) ...
          + (a(5)/(5*w))*sin(5*w*t) - (b(5)/(5*w))*cos(5*w*t);
+
+pos = @(t) pos(t)-pos(0);
+
 g = diff(pos, t);
 i=1;
 sol = [];
-for n = 1:20
+for n = 1:40
     S = vpasolve(g==0,t,[-2 5],'Random',true);
     sol(i)=S;
     i = i+1;
@@ -32,12 +35,12 @@ acc_tmp(t) = diff(vel_tmp, t);
 acc = @(t) double(acc_tmp(t));
 
 time = 0:0.02:((2*pi)/w);
-save('time.mat', 'time');
+save('time'+i+'.mat', 'time');
 position = double(pos(time));
-save('pos.mat', 'position');
+save('pos'+i+'.mat', 'position');
 velocity = double(vel(time));
-save('vel.mat', 'velocity');
+save('vel'+i+'.mat', 'velocity');
 acceleration = double(acc(time));
-save('acc.mat', 'acceleration');
+save('acc'+i+'.mat', 'acceleration');
 
 end
