d9efb3fd18d95fd21edd62322f2f7e87ab6edd86
veronicavulcano <veronicavulcano@mbp-di-veronica>
2020-09-14 15:12:58 +0200

    function returning the Y matrix

--- first attempt/functions/Y_matrix.m
+++ first attempt/functions/Y_matrix.m
@@ -1,4 +1,4 @@
-syms q1 q2 q3 dq1 dq2 dq3 ddq1 ddq2 ddq3
+function [Y] = Y_matrix(q1, q2, q3, dq1, dq2, dq3, ddq1, ddq2, ddq3)
 
 Y11 = ddq1
 Y12 = cos(q2)^2*ddq1-dq1*dq2*sin(2*q2)
@@ -42,6 +42,7 @@ Y311 = 0
 Y312 = cos(q2+q3)
 Y313 = 0
 
-Y = simplify([Y11 Y12 Y13 Y14 Y15 Y16 Y17 Y18 Y19 Y110 Y111 Y112 Y113;
+Y = [Y11 Y12 Y13 Y14 Y15 Y16 Y17 Y18 Y19 Y110 Y111 Y112 Y113;
      Y21 Y22 Y23 Y24 Y25 Y26 Y27 Y28 Y29 Y210 Y211 Y212 Y213;
-     Y31 Y32 Y33 Y34 Y35 Y36 Y37 Y38 Y39 Y310 Y311 Y312 Y313])
+     Y31 Y32 Y33 Y34 Y35 Y36 Y37 Y38 Y39 Y310 Y311 Y312 Y313]
+end
