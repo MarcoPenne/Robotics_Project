981abce50b4f0b4ab7782f4810de642ae90dc3e8
veronicavulcano <veronicavulcano@mbp-di-veronica>
2020-10-08 19:58:33 +0200

    filtered torque added and simulator changed

--- first attempt/Simulation_3dof_new.m
+++ first attempt/Simulation_3dof_new.m
@@ -1,4 +1,6 @@
+clc
 clear all
+close all
 addpath("functions/");
 
 if ismac
@@ -14,7 +16,7 @@ clientID=sim.simxStart('127.0.0.1',19997,true,true,5000,5);
 
 if (clientID>-1)
     disp('Connected to remote API server');
-    [returnCode]=sim.simxLoadScene(clientID,'scenes/3-dof_position_control.ttt',1,sim.simx_opmode_blocking)
+    [returnCode]=sim.simxLoadScene(clientID,'scenes/3-dof_position_control_vortex.ttt',1,sim.simx_opmode_blocking)
     display(returnCode);
     
     % Now send some data to CoppeliaSim in a non-blocking fashion:
@@ -29,17 +31,33 @@ if (clientID>-1)
 else
     disp('Failed connecting to remote API server');
 end
+ 
+% [x1, y1, z1] = looping_splines([0 pi/2 -pi/2 pi/2 0],[0 5 10 15 20]);
+% [x2, y2, z2] = looping_splines([-pi/2 -pi/4 -pi/2],[0 10 20]);
+% [x3, y3, z3] = looping_splines([0 pi/4 -pi/3 0],[0 5 15 20]);
 
-[x1, y1, z1] = looping_splines([0 pi/2 0 -pi/2 0],[0 5 10 15 20]);
+[x1, y1, z1] = looping_splines([0 pi/4 -pi/3 0],[0 5 15 20]);
 [x2, y2, z2] = looping_splines([-pi/2 -pi/4 -pi/2],[0 10 20]);
 [x3, y3, z3] = looping_splines([0 pi/4 -pi/3 0],[0 5 15 20]);
 
 % [x1, y1, z1] = looping_splines([-pi/2 -pi/4 -pi/2],[0 10 20]);
 % [x2, y2, z2] = looping_splines([-pi/2 -pi/4 -pi/2],[0 10 20]);
 % [x3, y3, z3] = looping_splines([-pi/2 -pi/4 -pi/2],[0 10 20]);
+ 
+% [position1, velocity1, acceleration1, time1] = load_trajectory('data/3-dof/trajectory1');
+% [position2, velocity2, acceleration2, time2] = load_trajectory('data/3-dof/trajectory2');
+% [position3, velocity3, acceleration3, time3] = load_trajectory('data/3-dof/trajectory3');
 % 
+% time_tmp = [time1; time2; time3];
+% position_tmp = [position1; position2; position3];
+% velocity_tmp = [velocity1; velocity2; velocity3];
+% acceleration_tmp = [acceleration1; acceleration2; acceleration3];
+% 
+% [x1, y1, z1] = looping_splines(position_tmp(1,:), time_tmp(1,:));
+% [x2, y2, z2] = looping_splines(position_tmp(2,:), time_tmp(2,:));
+% [x3, y3, z3] = looping_splines(position_tmp(3,:), time_tmp(3,:));
 
-%period = double(lcm(sym([time_tmp(1, size(time_tmp, 2)), time_tmp(2, size(time_tmp, 2)), time_tmp(3, size(time_tmp, 2))])));
+% period = double(lcm(sym([time_tmp(1, size(time_tmp, 2)), time_tmp(2, size(time_tmp, 2)), time_tmp(3, size(time_tmp, 2))])));
 period = 20;
 
 position = {x1, x2, x3};
@@ -72,9 +90,9 @@ while seconds(datetime('now')-start) < period
     t = seconds(datetime('now')-start);
     time_axis(i) = t;
     
-    [returnCode,joint_position1]=sim.simxGetJointPosition(clientID,joints(1),sim.simx_opmode_blocking);
-    [returnCode,joint_position2]=sim.simxGetJointPosition(clientID,joints(2),sim.simx_opmode_blocking);
-    [returnCode,joint_position3]=sim.simxGetJointPosition(clientID,joints(3),sim.simx_opmode_blocking);
+    [returnCode,joint_position1]=sim.simxGetJointPosition(clientID,joints(1),sim.simx_opmode_streaming);
+    [returnCode,joint_position2]=sim.simxGetJointPosition(clientID,joints(2),sim.simx_opmode_streaming);
+    [returnCode,joint_position3]=sim.simxGetJointPosition(clientID,joints(3),sim.simx_opmode_streaming);
     measured_pos(:, i) = [joint_position1; joint_position2; joint_position3];
     
     % Sending commands
@@ -87,13 +105,14 @@ while seconds(datetime('now')-start) < period
     reference_vel(:,i) = [velocity{1}(t); velocity{2}(t); velocity{3}(t)];
     reference_acc(:,i) = [acceleration{1}(t); acceleration{2}(t); acceleration{3}(t)];
     
-    [returnCode,u1]=sim.simxGetJointForce(clientID,joints(1),sim.simx_opmode_blocking);
-    [returnCode,u2]=sim.simxGetJointForce(clientID,joints(2),sim.simx_opmode_blocking);
-    [returnCode,u3]=sim.simxGetJointForce(clientID,joints(3),sim.simx_opmode_blocking);
+    [returnCode,u1]=sim.simxGetJointForce(clientID,joints(1),sim.simx_opmode_streaming);
+    [returnCode,u2]=sim.simxGetJointForce(clientID,joints(2),sim.simx_opmode_streaming);
+    [returnCode,u3]=sim.simxGetJointForce(clientID,joints(3),sim.simx_opmode_streaming);
     measured_torque(:, i) = [-u1; -u2; -u3];
     
     frame_period = seconds(datetime('now')-start)/i;
     i=i+1;
+    pause(0.02);
     
 end
 
@@ -125,16 +144,16 @@ filtered_vel(2, :) = filtfilt(filt, estimated_velocity(2,:));
 filtered_vel(3, :) = filtfilt(filt, estimated_velocity(3,:));
 
 subplot(3,3,4);
-plot(time_axis(1, 1:i-1), reference_vel(1, 1:i-1), time_axis(1, 1:i-1), filtered_vel(1, 1:i-1));
-legend ("reference vel j1", "velocity j1")
+plot(time_axis(1, 1:i-1), reference_vel(1, 1:i-1), time_axis(1, 1:i-1), estimated_velocity(1, 1:i-1), time_axis(1, 1:i-1), filtered_vel(1, 1:i-1));
+legend ("reference vel j1", "estimated velocity j1", "filtered velocity j1")
 
 subplot(3,3,5);
-plot(time_axis(1, 1:i-1), reference_vel(2, 1:i-1), time_axis(1, 1:i-1), filtered_vel(2, 1:i-1));
-legend ("reference vel j2", "velocity j2")
+plot(time_axis(1, 1:i-1), reference_vel(2, 1:i-1), time_axis(1, 1:i-1), estimated_velocity(2, 1:i-1), time_axis(1, 1:i-1), filtered_vel(2, 1:i-1));
+legend ("reference vel j2", "estimated velocity j2", "filtered velocity j2")
 
 subplot(3,3,6);
-plot(time_axis(1, 1:i-1), reference_vel(3, 1:i-1), time_axis(1, 1:i-1), filtered_vel(3, 1:i-1));
-legend ("reference vel j3", "velocity j3");
+plot(time_axis(1, 1:i-1), reference_vel(3, 1:i-1), time_axis(1, 1:i-1), estimated_velocity(3, 1:i-1), time_axis(1, 1:i-1), filtered_vel(3, 1:i-1));
+legend ("reference vel j3", "estimated velocity j3", "filtered velocity j3");
 
 estimated_acceleration = [gradient(gradient(measured_pos(1, 1:i-1),frame_period),frame_period); gradient(gradient(measured_pos(2, 1:i-1),frame_period),frame_period); gradient(gradient(measured_pos(3, 1:i-1),frame_period),frame_period)];
 
@@ -144,42 +163,47 @@ filtered_acc(2, :) = filtfilt(filt, estimated_acceleration(2,:));
 filtered_acc(3, :) = filtfilt(filt, estimated_acceleration(3,:));
 
 subplot(3,3,7);
-plot(time_axis(1, 1:i-1), reference_acc(1, 1:i-1), time_axis(1, 1:i-1), filtered_acc(1, 1:i-1));
-legend ("reference acc j1", "acceleration j1")
+plot(time_axis(1, 1:i-1), reference_acc(1, 1:i-1), time_axis(1, 1:i-1), estimated_acceleration(1, 1:i-1), time_axis(1, 1:i-1), filtered_acc(1, 1:i-1));
+legend ("reference acc j1", "estimated acc j1", "filtered acceleration j1")
 
 subplot(3,3,8);
-plot(time_axis(1, 1:i-1), reference_acc(2, 1:i-1), time_axis(1, 1:i-1), filtered_acc(2, 1:i-1));
-legend ("reference acc j2", "acceleration j2")
+plot(time_axis(1, 1:i-1), reference_acc(2, 1:i-1), time_axis(1, 1:i-1), estimated_acceleration(2, 1:i-1), time_axis(1, 1:i-1), filtered_acc(2, 1:i-1));
+legend ("reference acc j2", "estimated acc j2", "filtered acceleration j2")
 
 subplot(3,3,9);
-plot(time_axis(1, 1:i-1), reference_acc(3, 1:i-1), time_axis(1, 1:i-1), filtered_acc(3, 1:i-1));
-legend ("reference vel j3", "acceleration j3");
+plot(time_axis(1, 1:i-1), reference_acc(3, 1:i-1), time_axis(1, 1:i-1), estimated_acceleration(3, 1:i-1), time_axis(1, 1:i-1), filtered_acc(3, 1:i-1));
+legend ("reference vel j3", "estimated acc j3", "filtered acceleration j3");
 
 for j=1:i-1
     Y = Y_matrix(reference_pos(1, j), reference_pos(2, j), reference_pos(3, j), reference_vel(1, j), reference_vel(2, j), reference_vel(3, j), reference_acc(1, j), reference_acc(2, j), reference_acc(3, j));
-    a = get_3dof_coefficients(1.5, 0, -0.15, 0, 4.167e-04*1.5, 1.125, -0.15, 0, -0.06, 4.167e-04*1.125, 7.708e-03*1.125, 7.708e-03*1.125, 0.75, -0.1, 0, 0, 4.167e-04*0.75, 3.542e-03*0.75, 3.542e-03*0.75);
+    a = get_3dof_coefficients(10, 0, -0.15, 0, 4.167e-04*10, 1.125, -0.15, 0, -0.06, 4.167e-04*1.125, 7.708e-03*1.125, 7.708e-03*1.125, 0.75, -0.1, 0, 0, 4.167e-04*0.75, 3.542e-03*0.75, 3.542e-03*0.75);
     reference_torque(:, j) = Y*a;
 end
 
+filtered_torque = zeros(3, length(measured_torque));
+filtered_torque(1, :) = filtfilt(filt, measured_torque(1,:));
+filtered_torque(2, :) = filtfilt(filt, measured_torque(2,:));
+filtered_torque(3, :) = filtfilt(filt, measured_torque(3,:));
+
 figure('Name', 'Torques');
 subplot(3,1,1);
-plot(time_axis(1, 1:i-1), measured_torque(1, 1:i-1), time_axis(1, 1:i-1), reference_torque(1, 1:i-1));
-legend ("torque j1", "reference torque1")
+plot(time_axis(1, 1:i-1), measured_torque(1, 1:i-1), time_axis(1, 1:i-1), reference_torque(1, 1:i-1), time_axis(1, 1:i-1), filtered_torque(1, 1:i-1));
+legend ("measured torque j1", "reference torque1", "filtered torque1")
 
 subplot(3,1,2);
-plot(time_axis(1, 1:i-1), measured_torque(2, 1:i-1), time_axis(1, 1:i-1), reference_torque(2, 1:i-1));
-legend ("torque j2", "reference torque2")
+plot(time_axis(1, 1:i-1), measured_torque(2, 1:i-1), time_axis(1, 1:i-1), reference_torque(2, 1:i-1), time_axis(1, 1:i-1), filtered_torque(2, 1:i-1));
+legend ("measured torque j2", "reference torque2", "filtered torque2")
 
 subplot(3,1,3);
-plot(time_axis(1, 1:i-1), measured_torque(3, 1:i-1), time_axis(1, 1:i-1), reference_torque(3, 1:i-1));
-legend ("torque j3", "reference torque3");
+plot(time_axis(1, 1:i-1), measured_torque(3, 1:i-1), time_axis(1, 1:i-1), reference_torque(3, 1:i-1), time_axis(1, 1:i-1), filtered_torque(3, 1:i-1));
+legend ("measured torque j3", "reference torque3", "filtered torque3");
 
 Y_stack = zeros((i-1)*3,13);
 u_stack = zeros((i-1)*3,1);
 
 for k=1:(i-1)
     Y_stack(k*3-2:k*3,:) = Y_matrix(measured_pos(1,k),measured_pos(2,k),measured_pos(3,k),filtered_vel(1,k),filtered_vel(2,k),filtered_vel(3,k),filtered_acc(1,k),filtered_acc(2,k),filtered_acc(3,k));
-    u_stack(k*3-2:k*3,:) = measured_torque(:,k);
+    u_stack(k*3-2:k*3,:) = filtered_torque(:,k);
 end
 
 coefficients = pinv(Y_stack)*u_stack;
