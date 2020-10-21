00db0be1c07817b4843b89f2a683fa50b4e05931
MarcoPenne <marco.pennese.97@gmail.com>
2020-10-21 19:38:03 +0200

    beatify code

--- Code/PBRP_1dof_tree.m
+++ Code/PBRP_1dof_tree.m
@@ -1,15 +1,3 @@
-% author: Claudio Gaz, Marco Cognetti
-% date: August 2, 2019
-% 
-% -------------------------------------------------
-% Parameters Retrieval Algorithm
-% -------------------------------------------------
-% C. Gaz, M. Cognetti, A. Oliva, P. Robuffo Giordano, A. De Luca, 'Dynamic
-% Identification of the Franka Emika Panda Robot With Retrieval of Feasible
-% Parameters Using Penalty-Based Optimization'. IEEE RA-L, 2019.
-%
-% the following code has been tested on Matlab 2018b
-
 close all
 clc
 clear all
