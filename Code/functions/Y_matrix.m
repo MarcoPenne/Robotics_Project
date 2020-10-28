799163753dca6cf070b364310e3633bd6efa742f
MarcoPenne <marco.pennese.97@gmail.com>
2020-10-28 18:39:44 +0100

    updated minimal representation

--- Code/functions/Y_matrix.m
+++ Code/functions/Y_matrix.m
@@ -2,9 +2,9 @@ function [Y] = Y_matrix(q1, q2, q3, dq1, dq2, dq3, ddq1, ddq2, ddq3)
 
 Y11 = ddq1;
 Y12 = cos(q2)^2*ddq1-dq1*dq2*sin(2*q2);
-Y13 = sin(q2)^2*ddq1+dq1*dq2*sin(2*q2);
+Y13 = ddq1;
 Y14 = cos(q2+q3)^2*ddq1-dq1*dq2*sin(2*q2+2*q3)-dq1*dq3*sin(2*q2+2*q3);
-Y15 = sin(q2+q3)^2*ddq1+dq1*dq2*sin(2*q2+2*q3)+dq1*dq3*sin(2*q2+2*q3);
+Y15 = ddq1;
 Y16 = 2*cos(q2+q3)*cos(q2)*ddq1-dq1*dq3*sin(q3)-2*dq1*dq2*sin(2*q2+q3)-dq1*dq3*sin(2*q2+q3);
 Y17 = 0;
 Y18 = 0;
@@ -16,9 +16,9 @@ Y113 = 0;
 
 Y21 = 0;
 Y22 = (dq1^2*sin(2*q2))/2;
-Y23 = -(dq1^2*sin(2*q2))/2;
+Y23 = 0;
 Y24 = (dq1^2*sin(2*q2+2*q3))/2;
-Y25 = -(dq1^2*sin(2*q2+2*q3))/2;
+Y25 = 0;
 Y26 = cos(q3)*(2*ddq2+ddq3)-2*dq2*dq3*sin(q3)+dq1^2*sin(2*q2+q3)-dq3^2*sin(q3);
 Y27 = ddq2;
 Y28 = ddq3;
@@ -32,7 +32,7 @@ Y31 = 0;
 Y32 = 0;
 Y33 = 0;
 Y34 = (dq1^2*sin(2*q2+2*q3))/2;
-Y35 = -(dq1^2*sin(2*q2+2*q3))/2;
+Y35 = 0;
 Y36 = cos(q3)*ddq2+dq2^2*sin(q3)+(dq1^2*sin(q3))/2+(dq1^2*sin(2*q2+q3))/2;
 Y37 = 0;
 Y38 = ddq2+ddq3;
@@ -42,7 +42,8 @@ Y311 = 0;
 Y312 = cos(q2+q3);
 Y313 = 0;
 
-Y = [Y11 Y12 Y13 Y14 Y15 Y16 Y17 Y18 Y19 Y110 Y111 Y112 Y113;
-     Y21 Y22 Y23 Y24 Y25 Y26 Y27 Y28 Y29 Y210 Y211 Y212 Y213;
-     Y31 Y32 Y33 Y34 Y35 Y36 Y37 Y38 Y39 Y310 Y311 Y312 Y313];
+% columns 11 and 13 are equal
+Y = [Y11 Y12 Y14 Y16 Y17 Y18 Y19 Y110 Y111 Y112;
+     Y21 Y22 Y24 Y26 Y27 Y28 Y29 Y210 Y211 Y212;
+     Y31 Y32 Y34 Y36 Y37 Y38 Y39 Y310 Y311 Y312];
 end
--- Code/functions/get_3dof_coefficients.m
+++ Code/functions/get_3dof_coefficients.m
@@ -10,20 +10,20 @@ d2 = 0.09;
 L2 = 0.3;
 L3 = 0.2;
 
-a1= I1yy + m2*(c+ d2)^2 + m3 * d2^2;
-a2 = m2*(L2 -b)^2 + m3 * L2^2 + I2yy;
-a3 = I2xx;
-a4 = I3yy + m3*(L3 - d)^2;
-a5 = I3xx;
+a1= I1yy + m2*(c+ d2)^2 + m3 * d2^2 + I2xx + I3xx;
+a2 = m2*(L2 -b)^2 + m3 * L2^2 + I2yy - I2xx;
+a3 = I2xx; % 3 subtracted to 2 and added to 1
+a4 = I3yy + m3*(L3 - d)^2 - I3xx;
+a5 = I3xx; % 5 subtracted to 4 and added to 1
 a6 = m3*(L3 -d)*L2;
 a7 = I2zz + m2*(L2 - b)^2 + I3zz + m3 * (L2^2 + (L3 - d)^2 );
 a8 = m3*(L3 -d)^2 + I3zz;
 a9 = m2*(L2 - b)*(c+ d2) + m3*L2*d2;
 a10 = m3*(L3 - d)*d2;
-a11 = g0 * m2 * (L2 - b);
+a11 = g0 * m2 * (L2 - b) + g0 * m3 * L2;
 a12 = g0 * m3 * (L3 - d);
-a13 = g0 * m3 * L2;
+a13 = g0 * m3 * L2; % 13 added to 11
 
-a = [a1; a2; a3; a4; a5; a6; a7; a8; a9; a10; a11; a12; a13];
+a = [a1; a2; a4; a6; a7; a8; a9; a10; a11; a12];
 
 end
