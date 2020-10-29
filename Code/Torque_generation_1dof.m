6920457f4cde5b68823f694fdb3ad91bbf76b525
veronicavulcano <veronicavulcano@mbp-di-veronica>
2020-10-29 10:11:18 +0100

    added script for generating 1-dof torque

---
+++ Code/Torque_generation_1dof.m
@@ -0,0 +1,64 @@
+clear all
+close all
+clc
+addpath("functions/");
+
+%% ----------------------
+% GENERATE EXCITING TRAJECTORY
+%------------------------
+
+[position, velocity, acceleration, time] = load_trajectory('data/1-dof/trajectory1');
+
+[position, velocity, acceleration] = looping_splines(position, time);
+
+period = time(length(time));
+
+figure('Name', 'Trajectories');
+subplot(3,1,1);
+fplot(position, [0 period]);
+xlabel("time (s)")
+ylabel("position (rad)")
+subplot(3,1,2);
+fplot(velocity, [0 period]);
+xlabel("time (s)")
+ylabel("velocity (rad/s)")
+subplot(3,1,3);
+fplot(acceleration, [0 period]);
+xlabel("time (s)")
+ylabel("acceleration (rad/s^2)");
+
+dt = 0.02;
+timestamps = 0:dt:period;
+
+freq = 1/dt;
+
+pos = position(timestamps);
+
+vel = velocity(timestamps);
+
+acc = acceleration(timestamps);
+
+Y_stack = zeros((length(timestamps)),2);
+u_stack = zeros((length(timestamps)),1);
+
+for j=1:length(timestamps)
+    Y = Y_matrix_1dof(pos(j), acc(j));
+    a = get_1dof_coefficients(5,0.5,5*(8.417e-02));
+    torque(j) = Y*a;
+    
+    Y_stack(j,:) = Y;
+    u_stack(j,:) = torque(j);
+end
+
+figure('Name', 'Torques');
+plot(timestamps, torque);
+xlabel("time (s)");
+ylabel("torque (Nm)");
+
+a
+coefficients = pinv(Y_stack)*u_stack
+error = norm(coefficients-a)
+
+experiment_path = 'data/1-dof/new_experiment1';
+save(fullfile(experiment_path,'Y_stack.mat'), 'Y_stack');
+save(fullfile(experiment_path,'u_stack.mat'), 'u_stack');
